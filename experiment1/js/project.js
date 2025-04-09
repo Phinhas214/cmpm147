6752ec67774bf01dccf0aee67d300a4c180723f2
Phinhas Asmelash <phinhasasmelash@rachelcarlson-100-64-79-120.resnet.ucsc.edu>
2025-04-08 22:54:36 -0700

    🚀 added text generation js logic

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,12 +23,55 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+  const fillers = {
+    introduce: ["Hello" , "Hi", "Sup", "G'day", "Good morning", "Good evening", "Greetings", "Salutations"],
+    action: ["arrested after driving car into the ocean", 
+             "impersonates security to get into Taylor Swift concert", 
+             "had hours-long junk food feast inside closed Walgreens", 
+             "takes on bizarre challenge of eating raw chicken every day", 
+             "saves neighbor from jaws of 11-foot gator",
+             "arrested after scaling cell tower",
+             "leads deputies on chase in stolen ambulance"
+            ],
+    man: ["Man", "Woman", "Guy", "Boy", "Girl", "Dog", "Aligator", "Cat", "Hamster", "Shark"]
+    
+  };
+  
+  const template = `$introduce! Have you heard about Florida $man $action`;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    // box.innerText = story;
+    $("#box").text(story);
+    
 
-  // call a method on the instance
-  myInstance.myMethod();
+  }
+  
+  /* global clicker */
+  // clicker.onclick = generate;
+  $("#clicker").click(generate);
+  
+  generate();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
